4ae4dfe5fad83872e0ff5ae3e31bfad9ef6dd1bc
Administrator <Administrator@QSNM4IRAYKXPZ7V>
2018-09-05 22:58:18 +0700

    commit 1

---
+++ config/env-build.js
@@ -0,0 +1,23 @@
+window.APP_CONFIG = {
+    OAUTH_ENDPOINT: '__baseUrl.auth.api__',
+    CASE_MANAGEMENT_API_ENDPOINT: '__baseUrl.ims.api__',  
+     DEFAULT_LANDING_PAGE: 'dashboard.home',
+     GOOGLE_OAUTH_CLIENT_ID: '__google.auth.client.id__',
+     GOOGLE_OAUTH_REDIRECT_URI: '__baseUrl.ims.ui__',
+     DOCGEN_OAUTH_CLIENT_ID: 'IMSDocGen',
+     DOCGEN_ENDPOINT: '__baseUrl.docgen.ui__',
+     GOOGLE_CAPTCHA_KEY: '__captchaSecret__',
+     CURRENT_STAGE: '__env__}',
+     APP_VERSION: '__Release.ReleaseName__',
+     SECURE_MESSAGING_POLL_INTERVAL: 3000,
+     SUPPORTED_PRINT_FILE_TYPES: ['docx', 'jpg', 'jpeg', 'pdf', 'png'],
+     FEATURE_TOGGLES: {
+       DOCGEN_INTEGRATION: true
+     },
+     APP_LINK_IOS: 'https://itunes.apple.com/us/app/id1146683364',
+     APP_LINK_ANDROID: 'https://play.google.com/store/apps/details?id=com.bal.cobalt',
+     WYSIWYG_FILEMANAGER_ENDPOINT: '__baseUrl.roxy.ui__',
+     DOCUMENT_DOMAIN: '__domain__',
+     HEAP_APP_ID: '__heapAppId__'
+   };
+   
---
+++ config/env-dev.js
@@ -0,0 +1,21 @@
+window.APP_CONFIG = {
+    OAUTH_ENDPOINT: 'http://ims-auth',
+    CASE_MANAGEMENT_API_ENDPOINT: 'http://ims-api',  
+    DEFAULT_LANDING_PAGE: 'dashboard.home',
+    GOOGLE_OAUTH_CLIENT_ID: '486413698171-n13ojf7t5cqo0mt1vub5j54t7t6ofken.apps.googleusercontent.com',
+    GOOGLE_OAUTH_REDIRECT_URI: 'http://ims-auth',
+    DOCGEN_OAUTH_CLIENT_ID: 'IMSDocGen',
+    DOCGEN_ENDPOINT: 'http://docgen-dev.baldev.local',
+    GOOGLE_CAPTCHA_KEY: '6LfGqiUUAAAAAHqKysSRn5v1Z2DP89RXy4xDmgQP',
+    CURRENT_STAGE: 'DEV',
+    APP_VERSION: '%APP_VERSION%',
+    SECURE_MESSAGING_POLL_INTERVAL: 3000,
+    SUPPORTED_PRINT_FILE_TYPES: ['docx', 'jpg', 'jpeg', 'pdf', 'png'],
+    FEATURE_TOGGLES: {
+      DOCGEN_INTEGRATION: true
+    },
+    APP_LINK_IOS: 'https://itunes.apple.com/us/app/id1146683364',
+    APP_LINK_ANDROID: 'https://play.google.com/store/apps/details?id=com.bal.cobalt',
+    WYSIWYG_FILEMANAGER_ENDPOINT: 'http://docgen-filebrowser-dev.baldev.local/index.html',
+    DOCUMENT_DOMAIN: 'baldev.local'
+  };
